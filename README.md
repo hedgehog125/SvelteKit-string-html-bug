a16337a27ba761d7a5d6e41afcebc0b0b664d4e7
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-07-21 23:49:15 +0100

    Improved the descriptions and added a new demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Sveltekit-string-html-bug
-This is a simple demo of a bug in SvelteKit where string literals are half treated as HTML. This issue happens when you build with a base path and include an escaped link that doesn't start with the base.
+This is a simple demo of a bug in SvelteKit where string literals are half treated as HTML. This issue happens when you build with a base path try and include the HTML for an anchor tag in plain text (so the you can read the code instead of seeing a link) in a page.
 
-Normally I'd use the repository name as the base path so I can host on GitHub pages, but I'm using "build" here to replicate the folder structure so you can test things.
+Normally I'd use the repository name as the base path so I can host on GitHub pages, but I'm using "build" here to replicate the folder structure so you can test things. For this reason, make sure you host it as stated below.
 
 # Demo
-Have a look at the static/src/routes/index.svelte and lib/Demo.js files. I've documented which lines produce errors but you can try removing or commenting out different lines to see yourself. Note that dev mode won't test it, as I've written some code to disable the base path in that mode. So that you can actually see the problematic lines, I've used HTML comments on them so it can compile.
+Have a look at the static/src/routes/index.svelte and static/src/lib/Demo.js files. Also host the static folder with something like http-server, and go to /build to test. I've already documented which lines cause issues there, but you can try removing or commenting out different lines to see yourself. Note that dev mode won't test it, as I've written some code to disable the base path in that mode (in svelte.config.js), so host as described earlier and build each time. I've also used HTML comments around the broken ones so the page can actually compile.
--- static/src/routes/index.svelte
+++ static/src/routes/index.svelte
@@ -1,45 +1,58 @@
 <script>
-	import linkPage from "$util/LinkPage.js";
+	import { onMount } from "svelte";
+	import linkPage from "$util/LinkPage.js"; // Not actually used, but it does mean that the escaped code would be valid if unescaped
 
 	import { textboxes } from "$lib/Demo.js";
+
+	let wildcardContent = "";
+
+	onMount(_ => {
+		wildcardContent = textboxes.demo;
+	});
 </script>
 
 <main>
 	<p>
-		Here are a bunch of variations of escaped HTML anchor tags, in both paragraphs (where tag escaping is needed) and in textareas (where it isn't).
+		Here are a bunch of variations of escaped HTML anchor tags in textareas. Paragraphs and presumably most other HTML elements don't have the same issue, I guess because the angle brackets are escaped before the links are checked.
 	</p>
 	<p>
-		When treated as literal html, the JavaScript will make the link start with the base path. But when it's being validatated, the code is often treated as the link itself, causing it to be invalid.
+		Although it's just supposed to show off some code, when it's treated as literal html, the JavaScript will make the link start with the base path. But when it's being validatated, the code is often treated as the link itself, causing it to be invalid as it doesn't start with the base path. Although weirdly even a valid hardcoded link seems to be treated as invalid. See below...
 	</p>
 	<p>
-		Here's an annoying way to escape it which actually works. So you can see the code: &lt;a href={`{linkPage("foo")}`}&gt;This works&lt;/a&gt; <br>
-		This page exists, which prevents some of the errors. That bit of code is just displayed in a p tag, using HTML escape codes for the greater than and less than symbols.
-	</p> <br>
+		The code that should be inside the various textareas is: &lt;a href={`{linkPage("foo")}`}&gt;This works&lt;/a&gt;
+	</p>
 	<p>
-		None of HTML in the JavaScript strings in the JavaScript files result in parsing errors. And escaping by using concatination to break up things like &lt;a&gt; doesn't make a difference.
+		None of the HTML stored in strings in the JavaScript file result in parsing errors. And escaping by using concatination to break up things like &lt;a&gt; doesn't make a difference (as the issue isn't in that file).
 	</p> <br>
 
 	<h1>The tests</h1>
 
 	<p>
-		*HTML commented* textarea with the content (that raw HTML code) from an external script (most likely to compile, but won't show anything), using Svelte curly brackets in the (commented) HTML:
+		Paragraph tag with the content (the HTML code that should be displayed as text) set using Svelte curly brackets to a string imported from a JavaScript file:
+	</p>
+	<p>
+		{textboxes.demo}
+	</p>
+	<p>
+		Compiles, which is presumably the same for most HTML elements (except some like textarea)
+	</p>
+
+	<p>
+		*Commented with HTML comment tag* textarea, with the content from an external script, using Svelte curly brackets in the (commented) HTML: (this is the most likely to compile, but won't show anything)
 	</p>
 	<!-- <textarea readonly cols="80" rows="2">{textboxes.demo}</textarea> <br> -->
 	<p>
 		Compiles
-	</p> <br>
+	</p>
 
 	<p>
-		Textarea with the content (that raw HTML code) from an external script, using Svelte curly brackets in the HTML:
+		Textarea with the content from an external script, using Svelte curly brackets in the HTML:
 	</p>
 	<!-- <textarea readonly cols="80" rows="2">{textboxes.demo}</textarea> <br> -->
 	<p>
 		*Doesn't* compile
-	</p> <br>
-
-	<p>
-		It's invalid syntax to run the same code but with a @html prefix on textboxes.hardCoded.
 	</p>
+
 	<p>
 		The same but the whole thing in a string but using @html (using vanilla string template): <br>
 		The contents is commented out by removing the dollar sign as I can't use an HTML comment.
@@ -48,11 +61,11 @@
 		<textarea readonly cols="80" rows="2">{textboxes.demo}</textarea> <br>
 	`}
 	<p>
-		*Doesn't* compile (unless the contents is removed like this)
+		*Doesn't* compile (unless the contents is removed in this way)
 	</p> <br>
 
 	<p>
-		Same but the URL in the escaped string uses a hardcoded prefix (meaning it would be valid even without JavaScript if it wasn't in a textarea). Still loaded from an external script and put into this document with Svelte curly brackets:
+		Same but the URL in the escaped string uses a hardcoded prefix (so the escaped HTML code would be valid if put unescaped without any JavaScript). Still loaded from an external script and put into this document with Svelte curly brackets:
 	</p>
 	<!-- <textarea readonly cols="80" rows="2">{textboxes.hardCoded}</textarea> <br> -->
 	<p>
@@ -78,6 +91,14 @@
 	</p> <br>
 
 	<p>
+		Same thing but using a non-existent property instead of the value property:
+	</p>
+	<textarea readonly cols="80" rows="2" foo={textboxes.demo}></textarea> <br>
+	<p>
+		Compiles (although no content because it's not set properly)
+	</p> <br>
+
+	<p>
 		Same thing but using a non-existent element but the commonly used "value" property:
 	</p>
 	<foo value={textboxes.demo}></foo>
@@ -96,7 +117,7 @@
 	<br>
 
 	<p>
-		HTML string put as an inline string in the HTML, using the {"{`<content...>`}"} method:
+		Content set without another script, using the {"{`<content...>`}"} method:
 	</p>
 	<!-- <textarea readonly cols="80" rows="2">{`<a href={linkPage("foo")}>This works</a>`}</textarea> <br> -->
 	<p>
@@ -104,7 +125,7 @@
 	</p> <br>
 
 	<p>
-		HTML string put directly in the document as the *inner content* of the tag:
+		HTML string put directly in the document as the inner content of the tag:
 	</p>
 	<!-- <textarea readonly cols="80" rows="2">
 		<a href={linkPage("foo")}>This works</a>
@@ -119,5 +140,13 @@
 	<!-- <textarea readonly cols="80" rows="2" value='<a href={linkPage("foo")}>This works</a>'></textarea> <br> -->
 	<p>
 		*Doesn't* compile
+	</p> <br>
+
+	<p>
+		Wildcard: setting the content on mount with Svelte curly brackets:
+	</p>
+	<textarea readonly cols="80" rows="2">{wildcardContent}</textarea> <br>
+	<p>
+		Compiles! Probably because it's not in the SSR so it can't be validated
 	</p> <br>
 </main>
